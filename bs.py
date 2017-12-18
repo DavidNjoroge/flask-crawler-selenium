f7669df972afd0fa20806e11a1e05cbb8c3e2b61
David Mwangi <njorogedavid13@gmail.com>
2017-12-18 19:29:09 +0300

    finished up on bs4 today livescore

--- bs.py
+++ bs.py
@@ -1,10 +1,21 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup
+#!usr/bin/env python3.6
 
-my_url='http://www.google.co.ke'
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url='http://www.bbc.co.uk/sport/football/scores-fixtures'
 
 uClient = uReq(my_url)
 page_html=uClient.read()
 uClient.close()
 page_soup = soup(page_html,"html.parser")
-page_soup.h1
+
+li=page_soup.findAll("li",{"class":"gs-u-pb-"})
+
+matches_list=[]
+for match in li:
+     col=match.findAll("span",{"class":"gs-u-display-none"})
+     matchDict={"home":col[0].text,"away":col[1].text}
+     matches_list.append(matchDict)
+
+print(matches_list)
